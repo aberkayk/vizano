5f40642374e65b178a4374e141691e57f597a2ec
aberkayk <aberkayk@gmail.com>
2024-11-19 09:33:55 +0300

    ASD

---
+++ src/app/[locale]/about-us/components/animated-content.tsx
@@ -0,0 +1,10 @@
+import { useTranslations } from "next-intl";
+import React from "react";
+
+const AnimatedContent = () => {
+  const t = useTranslations("AboutUsPage");
+
+  return <div>AnimatedContent</div>;
+};
+
+export default AnimatedContent;
