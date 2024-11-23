abb079cfad4f9d28b01b713a87bf69e125060d5d
aberkayk <aberkayk@gmail.com>
2024-11-23 17:58:05 +0300

    add jsonld

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import MobileNavbar from "@/components/mobile-navbar";
 import Footer from "@/components/footer";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
+import Head from "next/head";
 
 const font = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
@@ -15,6 +16,21 @@ const font = Roboto({
 export const metadata: Metadata = {
   title: "Ufuk Enerji",
   description: "Ufuk Enerji Elektrik Üretim Anonim Şirketi",
+};
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Organization", // Yerel bir iş yeri için "LocalBusiness" de seçebilirsiniz
+  name: "Ufuk Enerji Elektrik Üretim A.Ş.",
+  url: "https://ufukenerji.com.tr", // Sitenizin URL'si
+  logo: "https://ufukenerji.com.tr/square-logo.png", // Public klasöründeki logonuzun URL'si
+  contactPoint: {
+    "@type": "ContactPoint",
+    telephone: "+90 266 597 10 52", // Şirket telefon numarası
+    email: "ufukenerji@guclugrup.com", // Şirket e-posta adresi
+  },
+  description:
+    "Ufuk Enerji, 2015 yılında Balıkesir'in Kepsut ve Susurluk ilçelerinde kurulan Poyrazgölü Rüzgar Enerji Santrali ile Türkiye'nin yenilenebilir enerji kaynaklarına katkı sağlayan bir şirkettir.",
 };
 
 const locales = ["en"];
@@ -34,6 +50,12 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </Head>
       <body className={`${font.className} overflow-x-hidden`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <MobileNavbar />
